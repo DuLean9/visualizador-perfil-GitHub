e419c283a80fa831299a5a9bbcc5d7d9b34007df
DuLean9 <duilioleandro9@outlook.com>
2026-03-14 19:38:49 -0300

    feat: chamada da API, tratamento de erros e renderização dos dados de perfil na UI feitos

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,54 @@
 const inputSearch = document.querySelector("#input-search");
 const btnSearch = document.querySelector("#btn-search");
+const profileResults = document.querySelector(".profile-results");
 
 
-function handleSearch() {
+const baseUrl = 'https://api.github.com'
+
+async function getUser(userName) {
+    const response = await fetch(`${baseUrl}/users/${userName}`)
+
+    try{
+        
+        if (!response.ok) {
+            throw new Error('Usuário não encontrado')
+        }
+        
+        const data = await response.json()
+        console.log(data);
+        console.log(data.avatar_url)
+        console.log(data.name)
+        console.log(data.bio)
+        console.log(data.followers)
+        console.log(data.following)
+            
+            
+        return data
+
+    }  catch (error) {
+        console.error('Erro ao buscar perfil de usuário:', error);
+        alert('Usuário não encontrado')
+    }
+}
+
+async function handleSearch() {
     const searchValue = inputSearch.value
 
     if (searchValue) {
         console.log('O valor é:', searchValue);
+
+        const user = await getUser(searchValue)
+
+        if (user) {
+            profileResults.innerHTML = `
+                <div class="profile-card">
+                    <img src="${user.avatar_url}" alt="Avatar do ${user.name}" class="profile-avatar">
+                    <div class="profile-info">
+                        <h2>${user.name}</h2>
+                        <p>${user.bio || 'Sem bio cadastrada 😢'}</p>
+                    </div>
+                </div>`
+        }
         
     } else {
         alert('Digite um usuário no GitHub')
@@ -19,7 +61,7 @@ btnSearch.addEventListener('click', handleSearch);
 inputSearch.addEventListener('keyup', (e) => {
 
     if (e.key === 'Enter') {
-        handleSearch()
+        handleSearch();
     }
     
 });
